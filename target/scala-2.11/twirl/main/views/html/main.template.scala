4ffb52b3432466b87ceabb1ed165b74982694d92
Charles Ouyang <ch.k.ouyang@gmail.com>
2016-07-25 10:10:36 +0900

    Base

---
+++ target/scala-2.11/twirl/main/views/html/main.template.scala
@@ -0,0 +1,85 @@
+
+package views.html
+
+import play.twirl.api._
+import play.twirl.api.TemplateMagic._
+
+
+     object main_Scope0 {
+import models._
+import controllers._
+import play.api.i18n._
+import views.html._
+import play.api.templates.PlayMagic._
+import java.lang._
+import java.util._
+import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
+import play.core.j.PlayMagicForJava._
+import play.mvc._
+import play.data._
+import play.api.data.Field
+import play.mvc.Http.Context.Implicit._
+
+class main extends BaseScalaTemplate[play.twirl.api.HtmlFormat.Appendable,Format[play.twirl.api.HtmlFormat.Appendable]](play.twirl.api.HtmlFormat) with play.twirl.api.Template2[String,Html,play.twirl.api.HtmlFormat.Appendable] {
+
+  /*
+ * This template is called from the `index` template. This template
+ * handles the rendering of the page header and body tags. It takes
+ * two arguments, a `String` for the title of the page and an `Html`
+ * object to insert into the body of the page.
+ */
+  def apply/*7.2*/(title: String)(content: Html):play.twirl.api.HtmlFormat.Appendable = {
+    _display_ {
+      {
+
+
+Seq[Any](format.raw/*7.32*/("""
+
+"""),format.raw/*9.1*/("""<!DOCTYPE html>
+<html lang="en">
+    <head>
+        """),format.raw/*12.62*/("""
+        """),format.raw/*13.9*/("""<title>"""),_display_(/*13.17*/title),format.raw/*13.22*/("""</title>
+        <link rel="stylesheet" media="screen" href=""""),_display_(/*14.54*/routes/*14.60*/.Assets.versioned("stylesheets/main.css")),format.raw/*14.101*/("""">
+        <link rel="shortcut icon" type="image/png" href=""""),_display_(/*15.59*/routes/*15.65*/.Assets.versioned("images/favicon.png")),format.raw/*15.104*/("""">
+        <script src=""""),_display_(/*16.23*/routes/*16.29*/.Assets.versioned("javascripts/hello.js")),format.raw/*16.70*/("""" type="text/javascript"></script>
+    </head>
+    <body>
+        """),format.raw/*20.32*/("""
+        """),_display_(/*21.10*/content),format.raw/*21.17*/("""
+    """),format.raw/*22.5*/("""</body>
+</html>
+"""))
+      }
+    }
+  }
+
+  def render(title:String,content:Html): play.twirl.api.HtmlFormat.Appendable = apply(title)(content)
+
+  def f:((String) => (Html) => play.twirl.api.HtmlFormat.Appendable) = (title) => (content) => apply(title)(content)
+
+  def ref: this.type = this
+
+}
+
+
+}
+
+/*
+ * This template is called from the `index` template. This template
+ * handles the rendering of the page header and body tags. It takes
+ * two arguments, a `String` for the title of the page and an `Html`
+ * object to insert into the body of the page.
+ */
+object main extends main_Scope0.main
+              /*
+                  -- GENERATED --
+                  DATE: Mon Jul 25 10:08:57 JST 2016
+                  SOURCE: /Users/ouyangcharles/git/DemoPlay/app/views/main.scala.html
+                  HASH: b2ef05838e523ed9b12d7d4674a701ad3115bd7a
+                  MATRIX: 1002->260|1127->290|1155->292|1235->397|1271->406|1306->414|1332->419|1421->481|1436->487|1499->528|1587->589|1602->595|1663->634|1715->659|1730->665|1792->706|1886->862|1923->872|1951->879|1983->884
+                  LINES: 32->7|37->7|39->9|42->12|43->13|43->13|43->13|44->14|44->14|44->14|45->15|45->15|45->15|46->16|46->16|46->16|49->20|50->21|50->21|51->22
+                  -- GENERATED --
+              */
+          
