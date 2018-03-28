6696d37ee23d78694b86c3453ac6444826e65684
chao <chao@ouyang.io>
2018-03-28 14:54:32 +0800

    basic structure

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+export default [{
+    input: './index.js',
+    output: [{
+        file: 'dist/bundle.js',
+        format: 'cjs',
+    },],
+    plugins: [],
+    external: [
+        'fs',
+        'path',
+        'mkdirp',
+        'jsonfile',
+    ],
+},];
