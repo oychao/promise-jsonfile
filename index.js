76a72d5b28f9996737b740dbabc3512efad39f94
chao <chao@ouyang.io>
2018-03-28 16:41:59 +0800

    basic feature available

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log('hello world');
+import fs from 'fs';
+
+const read = (file, option) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(file, option, (err, data) => {
+            if (err) {
+                reject(err);
+            }
+            data = JSON.parse(stripBom(data));
+            resolve(data);
+        });
+    });
+};
+
+const write = (file, data, option) => {
+    return new Promise((resolve, reject) => {
+        try {
+            if (typeof data !== 'object') {
+                throw new TypeError('only object data supported');
+            }
+            data = JSON.stringify(data);
+            fs.writeFile(file, data, option, fileErr => {
+                if (fileErr) {
+                    reject(fileErr);
+                }
+                resolve();
+            });
+        } catch (typeErr) {
+            reject(typeErr);
+        }
+    });
+};
+
+function stripBom(content) {
+    if (Buffer.isBuffer(content)) {
+        content = content.toString('utf8');
+    }
+    return content.replace(/^\uFEFF/, '');
+}
+
+export default { read, write, };
--- rollup.config.js
+++ rollup.config.js
@@ -8,7 +8,6 @@ export default [{
     external: [
         'fs',
         'path',
-        'mkdirp',
         'jsonfile',
     ],
 },];
