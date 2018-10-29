134500a76b0a87313a4580f43d8d7ed56163728e
ouyang-chao <chao@ouyang.io>
2018-10-29 11:21:12 +0800

    [feature] refactor with typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,19 @@
-export default [{
-    input: './index.js',
-    output: [{
-        file: 'dist/bundle.js',
-        format: 'cjs',
-    },],
-    plugins: [],
-    external: [
-        'fs',
-        'path',
-        'jsonfile',
-    ],
-},];
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+  input: './src/index.ts',
+  output: [
+    {
+      file: 'bin/index.js',
+      format: 'cjs'
+    }
+  ],
+  plugins: [
+    typescript({
+      tsconfigOverride: {
+        compilerOptions: { declaration: true }
+      }
+    })
+  ],
+  external: ['fs', 'path', 'jsonfile']
+};
